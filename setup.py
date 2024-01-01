4ae76b52a2679f06eee185308d1ebcf98b829b1c
NarciSource <jwch11@gmail.com>
2024-01-02 01:19:19 +0900

    chore: readme 수정

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='student-ai',
     version='0.1',

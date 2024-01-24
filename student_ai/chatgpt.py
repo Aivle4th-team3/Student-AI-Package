4022c65b372870965a458ef9a87a6aef610be3f2
NarciSource <jwch11@gmail.com>
2024-01-24 18:39:15 +0900

    refactor: talk_to_bot 구현부 ai로 이동 ver 0.2.7

--- student_ai/chatgpt.py
+++ student_ai/chatgpt.py
@@ -37,7 +37,28 @@ class Chatbot():
             answer = response.choices[0].message.content
         return answer
 
-    def chat(self, msg, history):
+    def chat(self, message, history):
+        # 챗봇에 기억 저장소 역할로 보내줄 메시지
+        selected_messages = []
+        # 러프한 길이 제한
+        LEN_LIMIT = 10000
+
+        # 메시지 임베딩 벡터화
+        user_message_embedded = self.get_embedding(message)
+        if history:
+            embedded_history = np.array(msg.user_message_embedded for msg in history)
+            # 코사인 유사도 비교
+            # 각 임베딩된 벡터의 크기가 1이므로 분모 생략
+            cosine_similarity = np.dot(embedded_history, user_message_embedded)
+            # 유사도 높은 순서로 선택하려고 뒤집음
+            descent_idx = np.argsort(cosine_similarity)[::-1]
+            len_count = 0
+            for idx in descent_idx:
+                if len_count < LEN_LIMIT:
+                    current_message = history[idx.item()]
+                    selected_messages.append((current_message.user_message, current_message.bot_message))
+                    len_count += (len(current_message.user_message) + len(current_message.bot_message))
+
         prompt_message = [
             {
                 "role": "system",
@@ -53,11 +74,12 @@ class Chatbot():
                 "content": "선생님, 오셨군요! 선생님을 기다리고 있었어요!"
             }
         ]
-        messages = self.__make_message(msg, history, prompt_message)
+        messages = self.__make_message(message, selected_messages, prompt_message)
         answer = self.__talk2gpt(messages)
 
-        history.append((msg, answer))
-        return answer
+        bot_message_embedded = self.get_embedding(answer)
+
+        return answer, user_message_embedded, bot_message_embedded
 
     def eval_test(self, question, answer, test, result):
         test_paper = test(question)

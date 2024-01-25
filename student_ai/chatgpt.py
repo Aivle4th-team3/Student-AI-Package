3046470f676ddff968ccde5505b377b689f81012
NarciSource <jwch11@gmail.com>
2024-01-25 15:07:16 +0900

    refactor: 유사도 측정 구현 분리

--- student_ai/chatgpt.py
+++ student_ai/chatgpt.py
@@ -8,7 +8,7 @@ class Chatbot():
     def __init__(self, api_key):
         self.client = OpenAI(api_key=api_key)
 
-    def __make_message(self, msg, history=[], prompt_message=[]):
+    def __capsule_message(self, msg, history=[], prompt_message=[]):
         messages = []
 
         for h in history:
@@ -36,29 +36,35 @@ class Chatbot():
         else:
             answer = response.choices[0].message.content
         return answer
+    
+    def __measure_similarity(self, messages, target_vector):
+        print("that", messages, target_vector)
+        embedded_history = np.array([msg.user_message_embedded for msg in messages])
+        # 코사인 유사도 비교
+        # 각 임베딩된 벡터의 크기가 1이므로 분모 생략
+        print("here",embedded_history, target_vector)
+        cosine_similarity = np.dot(embedded_history, target_vector)
 
-    def chat(self, message, history):
+        return cosine_similarity
+    
+    def __filter_messages(self, messages, cosine_similarity):
         # 챗봇에 기억 저장소 역할로 보내줄 메시지
         selected_messages = []
         # 러프한 길이 제한
         LEN_LIMIT = 10000
 
-        # 메시지 임베딩 벡터화
-        user_message_embedded = self.get_embedding(message)
-        if history:
-            embedded_history = np.array(msg.user_message_embedded for msg in history)
-            # 코사인 유사도 비교
-            # 각 임베딩된 벡터의 크기가 1이므로 분모 생략
-            cosine_similarity = np.dot(embedded_history, user_message_embedded)
-            # 유사도 높은 순서로 선택하려고 뒤집음
-            descent_idx = np.argsort(cosine_similarity)[::-1]
-            len_count = 0
-            for idx in descent_idx:
-                if len_count < LEN_LIMIT:
-                    current_message = history[idx.item()]
-                    selected_messages.append((current_message.user_message, current_message.bot_message))
-                    len_count += (len(current_message.user_message) + len(current_message.bot_message))
+        # 유사도 높은 순서로 선택하려고 뒤집음
+        descent_idx = np.argsort(cosine_similarity)[::-1]
+        len_count = 0
+        for idx in descent_idx:
+            if len_count < LEN_LIMIT:
+                current_message = messages[idx.item()]
+                selected_messages.append((current_message.user_message, current_message.bot_message))
+                len_count += (len(current_message.user_message) + len(current_message.bot_message))
 
+        return selected_messages
+
+    def chat(self, message, history):
         prompt_message = [
             {
                 "role": "system",
@@ -74,12 +80,22 @@ class Chatbot():
                 "content": "선생님, 오셨군요! 선생님을 기다리고 있었어요!"
             }
         ]
-        messages = self.__make_message(message, selected_messages, prompt_message)
-        answer = self.__talk2gpt(messages)
+
+
+        # 메시지 임베딩 벡터화
+        embedded_message = self.get_embedding(message)
+
+        if history:
+            cosine_similarity = self.__measure_similarity(history, embedded_message)
+            selected_messages = self.__filter_messages(history, cosine_similarity)
+        else:
+            selected_messages = []
+        capsuled_messages = self.__capsule_message(message, selected_messages, prompt_message)
+        answer = self.__talk2gpt(capsuled_messages)
 
         bot_message_embedded = self.get_embedding(answer)
 
-        return answer, user_message_embedded, bot_message_embedded
+        return answer, embedded_message, bot_message_embedded
 
     def eval_test(self, question, answer, test, result):
         test_paper = test(question)

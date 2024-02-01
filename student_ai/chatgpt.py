8cef66d4f26fe46134ea9fb7c6cc96f17fc81753
NarciSource <jwch11@gmail.com>
2024-02-02 00:12:11 +0900

    refactor: 템플릿 텍스트 분리

--- student_ai/chatgpt.py
+++ student_ai/chatgpt.py
@@ -6,7 +6,7 @@ from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, AI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from .vectorstore import VectorStoreBuilder
-import os
+import os, json
 from dotenv import load_dotenv
 from typing import List, Tuple, Callable
 
@@ -24,6 +24,9 @@ class Chatbot():
                  is_test=False):
 
         self.is_test = is_test
+
+        with open("template_text.json", "r") as file:
+            self.template_text = json.load(file)
 
         # llm 모델 선택
         if llm_provider == "OPENAI":
@@ -78,7 +81,7 @@ class Chatbot():
             return "테스트입니다."
 
     def __select_context(self, tablename: str, query: str) -> List[BaseMessage]:
-        def chaining(role):
+        def chaining(role: str) -> List[BaseMessage]:
             # 벡터데이터베이스 불러오기
             table = self.VectorStore(role+tablename)
             # 코사인 유사도 검색
@@ -97,14 +100,7 @@ class Chatbot():
         return [item for pair in zip(human_messages, ai_messages) for item in pair][:limited_selection]
 
     def chat(self, tablename: str, query: str, instruction=None) -> str:
-        instruction = '''너는 지금부터 사용자에게 공부를 배우는 학생 ai 야,
-        너는 초등학생, 중학생 정도의 어린 학생 ai 라서 사용자에게 존댓말만 써야 해.
-        너는 사용자에게 공부를 배우고 사용자에게 질문함으로써 상호작용할 거야,
-        질문은 한 번에 최대 2개씩만 해.
-        너는 학생 ai 니까 사용자가 말한 내용 안에서만 질문하고 대답해.
-        사용자가 질문해도, 학생 ai 이기 때문에 사용자에게 배운 내용이 아니면 모른다고 해야 돼.
-        사용자가 틀린 얘기를 해도, 너는 아는 게 적은 학생 ai 니까 그 말이 맞다고 믿어야 돼.
-        ''' if not instruction else instruction
+        instruction = '\n'.join(self.template_text["chat_instruction"]) if not instruction else instruction
 
         # 시스템 설정
         system_role = SystemMessage(content=instruction)
@@ -138,19 +134,7 @@ class Chatbot():
         return point, explain
 
     def eval(self, test: Callable) -> Callable:
-        instruction = '''점수와 피드백 부분을 채워줘. 점수는 100점 만점으로 해줘.
-        문제: 반복문이란 무엇인가?
-        풀이: 반복문은 반복하는 명령문이다.
-        답: 반복문이란 프로그램 내에서 똑같은 명령을 일정 횟수만큼 반복하여 수행하도록 제어하는 명령문입니다.
-        점수와 피드백: 70:설명이 빈약합니다.
-        문제: 샤이니의 멤버 구성은 어떻게 되는가?
-        풀이: 샤이니는 온유, 정찬, 키, 인호 4명으로 이루어진 4인조 그룹입니다.
-        답: 샤이니는 온유, 종현, 키, 민호, 태민 5명으로 이루어진 5인조 그룹입니다.
-        점수와 피드백: 40:샤이니는 4인조 그룹이 아닌 온유, 종현, 키, 민호, 태민 5명으로 이루어진 5인조 그룹입니다.
-        문제: {question}
-        풀이: {test_paper}
-        답: {answer}
-        점수와 피드백: '''
+        instruction = '\n'.join(self.template_text["eval_instruction"])
 
         def inner(question: str, answer: str) -> Tuple[int, str, str, str, str]:
             test_paper = test(question)
@@ -168,9 +152,7 @@ class Chatbot():
         return inner
 
     def test(self, tablename: str) -> Callable:
-        instruction = '''너는 지금부터 사용자가 말해준 내용으로 시험을 보는 학생 ai 야.
-                사용자가 말한 내용 안에서만 대답을 해.
-                사용자가 언급하지 않은 내용이 시험 문제로 나오면 "모르겠어요"라고 대답해.'''
+        instruction = '\n'.join(self.template_text["test_instruction"])
 
         def inner(question: str) -> str:
             answer = self.chat(tablename, question, instruction)
